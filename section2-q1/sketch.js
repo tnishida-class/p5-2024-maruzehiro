c0edffc3e685fdd982ed576d7f6c360b1ec76748
maruzehiro <marhazahiyah@gmail.com>
2024-10-08 12:20:55 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,12 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    let d = (i + 1) * 10;
+    if (i < 5) {
+      stroke(0, 0, 255);
+    } else {
+      stroke(255, 0, 0);
+    }
+    ellipse(width/2, height/2, d, d);
   }
 }
