ef51a2d79c020719de34ffa222d02b1d92cf20fd
maruzehiro <marhazahiyah@gmail.com>
2024-10-08 12:23:56 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,21 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      let x = i * s;
+      let y = j * s;
+      if ((i + j) % 2 === 1) {
+        fill(122);
+        rect(x, y, s, s);
+      }
+      if (((i + j) % 2 === 1) && (j < 3)) {
+        fill(255, 0, 0);
+        ellipse(x + s / 2, y + s / 2, d);
+      }
+      if (((i + j) % 2 === 1) && (4 < j)) {
+        fill(0);
+        ellipse(x + s / 2, y + s / 2, d, d);
+      }
+
     }
   }
 }
