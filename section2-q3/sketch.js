b09d29f92a44e445464276e1e416c0525384d405
maruzehiro <marhazahiyah@gmail.com>
2024-10-22 08:45:28 +0900

    test q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -17,7 +17,64 @@ function setup() {
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
+  drawCircle(green, maxR * 0.07);
   drawCircle(red, maxR * 0.05);
+}
+
+
+function drawArcs(c1, c2, r) {
+  const cx = width / 2; // 中心は (cx, cy)
+  const cy = height / 2;
+  for (let i = 0; i < 20; i++) {
+    let start = TWO_PI / 20 * i;
+    let stop = TWO_PI / 20 * (i + 1);
+    fill(i % 2 == 0 ? c1 : c2);
+    arc(cx, cy, r, r, start, stop, PIE);
+  }
+}
+
+// arc 1
+function drawArcs(c1, c2, r) {
+  const cx = width / 2; // 中心は (cx, cy)
+  const cy = height / 2;
+  for (let i = 0; i < 20; i++) {
+    let start = TWO_PI / 20 * i;
+    let stop = TWO_PI / 20 * (i + 1);
+    fill(i % 2 == 0 ? c1 : c2);
+    arc(cx, cy, r, r, start, stop, PIE);
+  }
+}
+
+//arc 2
+function drawArcs(c1, c2, r) {
+  const cx = width / 2; // 中心は (cx, cy)
+  const cy = height / 2;
+  for (let i = 0; i < 20; i++) {
+    let start = TWO_PI / 20 * i;
+    let stop = TWO_PI / 20 * (i + 1);
+    fill(i % 2 == 0 ? c1 : c2);
+    arc(cx, cy, r, r, start, stop, PIE);
+  }
+}
+
+// arc 3
+function drawArcs(c1, c2, r) {
+  const cx = width / 2; // 中心は (cx, cy)
+  const cy = height / 2;
+  for (let i = 0; i < 20; i++) {
+    let start = TWO_PI / 20 * i;
+    let stop = TWO_PI / 20 * (i + 1);
+    fill(i % 2 == 0 ? c1 : c2);
+    arc(cx, cy, r, r, start, stop, PIE);
+  }
+}
+
+// bulat hijau
+function drawCircle(c, r){
+  const cx = width / 3; // 中心は (cx, cy)
+  const cy = height / 3;
+  fill(c);
+  ellipse(cx, cy, r, r);
 }
 
 function drawCircle(c, r){
@@ -25,26 +82,4 @@ function drawCircle(c, r){
   const cy = height / 2;
   fill(c);
   ellipse(cx, cy, r, r);
-}
-
-function drawArcs(c1, c2, r) {
-  const cx = width / 2; // 中心は (cx, cy)
-  const cy = height / 2;
-  for (let i = 0; i < 20; i++) {
-    let start = TWO_PI / 20 * i;
-    let stop = TWO_PI / 20 * (i + 1);
-    fill(i % 2 == 0 ? c1 : c2);
-    arc(cx, cy, r, r, start, stop, PIE);
-  }
-}
-
-function drawArcs(c1, c2, r) {
-  const cx = width / 2; // 中心は (cx, cy)
-  const cy = height / 2;
-  for (let i = 0; i < 20; i++) {
-    let start = TWO_PI / 20 * i;
-    let stop = TWO_PI / 20 * (i + 1);
-    fill(i % 2 == 0 ? c1 : c2);
-    arc(cx, cy, r, r, start, stop, PIE);
-  }
 }
