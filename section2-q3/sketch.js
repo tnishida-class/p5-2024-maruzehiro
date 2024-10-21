a40a0d333a2fb604ee81630389032ca0c2e60690
maruzehiro <marhazahiyah@gmail.com>
2024-10-22 08:46:01 +0900

    test q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -17,7 +17,7 @@ function setup() {
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
-  drawCircle(green, maxR * 0.07);
+  drawCircle(green, maxR * 0.1);
   drawCircle(red, maxR * 0.05);
 }
 
