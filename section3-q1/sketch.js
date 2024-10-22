4cbcf7d2995820afa76e9c9aad0fadc09ce5a3ea
maruzehiro <marhazahiyah@gmail.com>
2024-10-22 11:30:10 +0900

    3-1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,20 +1,20 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
-let size = 50;
-let count = 0;
-let cycle = 100;
-let increment = 1;
+let size = 50; // memula ni takde langsung
+let count = 0; // memula ni takde = 0
+let cycle = 100; // memula ni takde =100
+let increment = 1; // memula ni takde langsung
 
 function setup(){
   createCanvas(200, 200);
-  count = 0;
-  cycle = 100;
+  count = 0; //xde kat progtouch
+  cycle = 100; //xde kat progtouch
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + increment) % cycle;
+  count = (count + increment) % cycle; // memula count + 1, now count + increment
   if (keyIsPressed) {
-    increment = 2;
+    increment = 10; //memula =2
   }else{
     increment = 1;
   }
@@ -25,3 +25,7 @@ function draw(){
   }
   ellipse(width / 2, height / 2, size);
 }
+
+
+//memula = kalau tekan key, xde perubahan
+//now = kalau tekan key, dia makin laju
