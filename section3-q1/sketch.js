a27d7c85952a04752e8634e1b78f53db965363af
maruzehiro <marhazahiyah@gmail.com>
2024-10-22 11:17:46 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -11,6 +11,15 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
+  if (keyIsPressed) {
+    increment = 2;
+  }else{
+    increment = 1;
+  }
+  if (count < cycle/2) {
+    size = count + 50;
+  }else{
+    size = (cycle - count) + 50;
+  }
   ellipse(width / 2, height / 2, size);
 }
